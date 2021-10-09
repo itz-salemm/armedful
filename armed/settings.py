8204e1daea2b4d67161152eb494cc8b7234b0df3
It_salemm <salemolorundare@gmail.com>
2021-10-09 01:01:42 -0700

    initial'

--- armed/settings.py
+++ armed/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'armed.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -85,8 +86,19 @@ DATABASES = {
         'HOST': 'armed.cqxwip1ioxod.us-east-2.rds.amazonaws.com',
         'PORT': '5432'
     }
+}'''
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
+import dj_database_url 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
