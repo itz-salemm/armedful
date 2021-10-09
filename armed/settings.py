a8343bb48ddc62f53b709dacfacaf9bf0adba7bd
It_salemm <salemolorundare@gmail.com>
2021-10-09 13:51:22 -0700

    initial commit

--- armed/settings.py
+++ armed/settings.py
@@ -76,25 +76,25 @@ WSGI_APPLICATION = 'armed.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'armedd',
         'USER': 'armedd',
-        'PASSWORD': 'armed12345',
-        'HOST': 'armed.cqxwip1ioxod.us-east-2.rds.amazonaws.com',
+        'PASSWORD': 'armedd12345',
+        'HOST': 'armedd.cqxwip1ioxod.us-east-2.rds.amazonaws.com',
         'PORT': '5432'
     }
-}'''
+}
 
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
 import dj_database_url 
 db_from_env = dj_database_url.config(conn_max_age=600)
